779965938281a8714dbb7c3f9c70db8b9b3b2845
Abdullah Khan <23100312@lums.edu.pk>
2022-04-09 02:04:20 +0500

    made some additions in tailwaind css and mint nft js related to flex

--- pages/dashboard/mint-nft.js
+++ pages/dashboard/mint-nft.js
@@ -8,15 +8,35 @@ import DashboardLayout from "layouts/Dashboard.js";
 export default function Index() {
   return (
     <>
-      
+      {/* <link rel="stylesheet" href="style.css"></link> */}
       <section className="mt-48 pb-40">
         <div className="justify-center text-center flex flex-wrap mt-24">
           <div className="w-full md:w-6/12 px-12 md:px-4">
-            <h2 className="font-semibold text-4xl">
-              Mint NFTs
+            <h2 className="font-semibold text-4xl pb-16">
+              MINT NFT PRODUCT
             </h2>
           </div>
+            <div class="fcontainer">
+            <div class="fitemLarge item1">1</div>
+            <div>
+              <div class="fitemMedium item3">2</div>
+              <div class="fitemSmall item3">2a</div>
+              <div class="fitemSmall item3">2b</div>
+              <div class="fitemSmall item3">2c</div>
+            </div>          
+          </div>
         </div>
+      </section>
+      <section>
+        {/* <div class="fcontainer">
+          <div class="fitemLarge item1">1</div>
+          <div>
+            <div class="fitemMedium item3">2</div>
+            <div class="fitemSmall item3">2a</div>
+            <div class="fitemSmall item3">2b</div>
+            <div class="fitemSmall item3">2c</div>
+          </div>          
+        </div> */}
       </section>
 
     </>
--- pages/track.js
+++ pages/track.js
@@ -6,7 +6,7 @@ import IndexLayout from "layouts/Index.js";
 export default function Index() {
   return (
     <>
-
+      {/* Can also use my-32 instead of mt-48 and pb-40 */}
       <section className="mt-48 pb-40">
         <div className="justify-center text-center flex flex-wrap mt-24">
           <div className="w-full md:w-6/12 px-12 md:px-4">
@@ -16,6 +16,11 @@ export default function Index() {
             <p className="text-lg leading-relaxed mt-4 mb-4 text-blueGray-500">
               Scan the QR code on the backside of the product
             </p>
+            <div className="bg-purple-200 h-72 font-semibold text-2xl flex justify-center content-center">
+              <p>
+                Scan Area
+              </p>
+            </div>
           </div>
         </div>
       </section>
