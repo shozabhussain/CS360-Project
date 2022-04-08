4b53871b99c6c2185654e2d7c612fc64037972e5
shozabhussain <23100174@lums.edu.pk>
2022-04-08 05:34:08 +0500

    logo and orientation remaining

--- layouts/Dashboard.js
+++ layouts/Dashboard.js
@@ -11,11 +11,11 @@ export default function Admin({ children }) {
   return (
     <>
       <Sidebar />
-      <div className="relative md:ml-64 bg-blueGray-100">
+      <div className="relative md:ml-64">
         {/* <AdminNavbar /> */}
         {/* Header */}
         {/* <HeaderStats /> */}
-        <div className="px-4 md:px-10 mx-auto w-full -m-24">
+        <div className="px-4  mx-auto w-full -m-24">
           {children}
           {/* <FooterAdmin /> */}
         </div>
--- pages/edit-profile/index.js
+++ pages/edit-profile/index.js
@@ -1,37 +1,83 @@
 /* eslint-disable react/jsx-no-target-blank */
 import React from "react";
 
-//import DashboardLayout from "layouts/Dashboard.js";
+import DashboardLayout from "layouts/Dashboard.js";
 
 // Landing Page
 
 export default function Index() {
   return (
     <>
-      <div className="flex flex-wrap custom-txt-title mt-20">
+      <div className="justify-center text-center flex flex-wrap mt-24 custom-txt-title py-24">
         <a>
         Edit Profile
         </a>
       </div>
 
-      {/* <div className="flex-auto px-4 lg:px-10 py-10 pt-0">
-        <form>
-          <div className="flex flex-wrap">
-            <div class="w-full lg:w-6/12 px-4">
-              <div class="relative w-full mb-3">
-                <label className="block uppercase text-blueGray-600 text-xs font-bold mb-2" for="grid-password">
-                  Username
+      <div className="flex-auto flex px-6">
+        <form className="px-6">
+          <div className="flex flex-wrap px-6">
+            <div class="px-6">
+              <div class="relative px-6">
+                <label className="block custom-txt-title text-sm mb-1" for="grid-password">
+                  Display Name:
                 </label>
-                  <input type="text" class="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150" value="lucky.jesse">
+                  <input type="text" class="border-0 px-3 py-3 placeholder-transparent text-blueGray-600 bg-black custom-txt-title uppercase rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150" value="demo mint it">
                     </input>
               </div>
             </div>
+
+            <div class="relative px-6">
+                <label className="block custom-txt-title text-sm mb-1" for="grid-password">
+                  Display Name:
+                </label>
+                  <input type="text" class="border-0 px-3 py-3 placeholder-transparent text-blueGray-600 bg-black custom-txt-title uppercase rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150" value="demo mint it">
+                    </input>
+            </div>
           </div>
         </form>
-      </div> */}
+      </div>
+
+      <div className="flex-auto flex py-12">
+        <form className="">
+          <div className="flex flex-wrap pl-10">
+            <div class=" md:w-4/12 pl-10">
+              <div class="relative pl-10">
+                <label className="block custom-txt-title text-sm mb-1 " for="grid-password">
+                  Account Status:
+                </label>
+                  <input type="text" class=" border-0 px-3 py-3 placeholder-transparent text-blueGray-600 bg-black custom-txt-title rounded text-sm shadow focus:outline-none focus:ring  ease-linear transition-all duration-150" value="Manufacturer">
+                    </input>
+                    <div className="text-xs text ">
+                    *Note: Changing Account Status back to manufacturer will revoke your verified status (if have)
+                    </div>
+                      {/* <label className="block custom-txt-title text-sm mb-1" >
+                      *Note: Changing Account Status back to manufacturer will revoke your verified status (if have)
+                </label> */}
+              </div>
+            </div>
+
+            <div class="pl-10">
+              <div class="pl-10">
+                <label className="block custom-txt-title text-sm mb-1" for="grid-password">
+                  Connected Wallet:
+                </label>
+                  <input type="text" class="border-0 px-3 py-3 placeholder-transparent text-blueGray-600 custom-bg-white custom-txt-title uppercase rounded text-sm shadow focus:outline-none focus:ring  ease-linear transition-all duration-150" value="demo mint it">
+                    </input>
+                 </div>
+            </div>
+          </div>
+        </form>
+      </div>
+
+      <div className="justify-center pt-6 mt-24 flex">
+      <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded custom-bg-lightblue justify-center">
+        Save Changes
+      </button>
+      </div>
     </>
 
   );
 }
 
-// Index.layout = DashboardLayout;
+Index.layout = DashboardLayout;
