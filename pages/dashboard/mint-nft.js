b9a7860dd4d1cb5516eb5439113a88fdb317cf5e
Abdullah Khan <23100312@lums.edu.pk>
2022-04-13 06:45:29 +0500

    final design of minting nft done

--- pages/dashboard/mint-nft.js
+++ pages/dashboard/mint-nft.js
@@ -113,13 +113,18 @@ export default function Index() {
 	return (
 		<>
 			{/* <link rel="stylesheet" href="style.css"></link> */}
-			<section className="mt-48 pb-10">
+			<section className="mt-48 pb-10 bg-gray-900">
 				<div className="justify-center text-center flex flex-wrap mt-24">
-					<div className="w-full md:w-6/12 px-12 md:px-4">
-						<h2 className="font-semibold text-4xl pb-16 mt-12">MINT NFT PRODUCT</h2>
+					<div className="w-full md:w-6/12 px-12 md:px-4 mb-6">
+						<h2 className="font-semibold text-4xl pb-16 mt-12 text-white">MINT NFT PRODUCT</h2>
 					</div>
 					<div className="fcontainer  pb-6">
-						<img className="fitemLarge item1 font-semibold text-1xl" src={imgPreview}></img>
+						<img className="fitemLarge font-semibold text-1xl" src={imgPreview}>
+						</img>
+
+						<h1 className="-ml-64 mr-60 font-bold text-black">
+							Image Preview
+						</h1>
 						{/* <div>
 							<div className="fitemMedium item3 font-semibold text-1xl">Click to Upload an Image</div>
 							<h2 className="font-semibold text-2xl">Product Name:</h2>
@@ -129,26 +134,38 @@ export default function Index() {
 							<h2 className="font-semibold text-2xl">Manufacturing Date:</h2>
 							<div className="fitemSmall item3 font-semibold text-1xl">__/__/__</div>
 						</div> */}
-						<div className="text-left">
-							<div className="relative w-full mb-3">
-								<label className="block uppercase text-blueGray-600 text-xs font-bold mb-2" htmlFor="grid-password">
+						<div className="text-left mt-4">
+							<div className="relative w-full mb-5">
+								{/* <label className="block  text-white text-xs font-bold mb-2" htmlFor="grid-password"> */}
+								{/* <label className="fitemMedium font-semibold text-1xl" htmlFor="grid-password">
 									Click to Upload an Image
-								</label>
+								</label> */}
+								<div className="fitemMedium item3 font-semibold text-1xl -mb-4">
+									<h2 className="mt-4">
+										Click to
+									</h2>
+
+									<h1 className="font-bold text-2xl">
+										Upload an Image
+									</h1>
+								</div>
 								<input
 									type="file"
-									className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
+									// className="rounded-lg text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
+									// className = "fitemMedium item3 font-semibold text-1xl"
 									placeholder="NFT Image"
 									onChange={handleImageInput}
 								/>
 							</div>
 
 							<div className="relative w-full mb-3">
-								<label className="block uppercase text-blueGray-600 text-xs font-bold mb-2" htmlFor="grid-password">
-									Product Name
+								<label className="block  text-white text-s font-bold mb-2" htmlFor="grid-password">
+									{/* Product Name */}
+									<h2 className="font-bold text-1xl">Product Name:</h2>
 								</label>
 								<input
 									type="text"
-									className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
+									className="font-bold border-2 border-white px-3 py-3 placeholder-gray-500 text-white bg-black rounded-lg text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
 									placeholder="Enter product name"
 									value={productName}
 									onChange={(e) => setProductName(e.target.value)}
@@ -156,12 +173,12 @@ export default function Index() {
 							</div>
 
 							<div className="relative w-full mb-3">
-								<label className="block uppercase text-blueGray-600 text-xs font-bold mb-2" htmlFor="grid-password">
-									Category
+								<label className="block  text-white text-s font-bold mb-2" htmlFor="grid-password">
+									Category:
 								</label>
 								<input
 									type="text"
-									className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
+									className="font-bold border-2 border-white px-3 py-3 placeholder-gray-500 text-white bg-black rounded-lg text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
 									placeholder="Select category"
 									value={category}
 									onChange={(e) => setCategory(e.target.value)}
@@ -169,12 +186,12 @@ export default function Index() {
 							</div>
 
 							<div className="relative w-full mb-3">
-								<label className="block uppercase text-blueGray-600 text-xs font-bold mb-2" htmlFor="grid-password">
-									Manufacturing Date
+								<label className="block  text-white text-s font-bold mb-2" htmlFor="grid-password">
+									Manufacturing Date:
 								</label>
 								<input
 									type="date"
-									className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
+									className="font-bold border-2 border-white px-3 py-3 placeholder-gray-500 text-white bg-black rounded-lg text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
 									placeholder="Manufacturing Date"
 									value={manufacturingDate}
 									onChange={(e) => setManufacturingDate(e.target.value)}
@@ -183,8 +200,8 @@ export default function Index() {
 						</div>
 					</div>
 
-					<div className="w-full md:w-6/12 px-12 md:px-4">
-						<h2 className="font-semibold text-4xl mt-12">ATTRIBUTES</h2>
+					<div className="w-full md:w-6/12 px-12 md:px-4 mt-4 -mb-24">
+						<h2 className="font-semibold text-4xl mt-12 text-white">ATTRIBUTES</h2>
 					</div>
 
 					{/* <div className="fcontainerSecond mt-10">
@@ -228,14 +245,14 @@ export default function Index() {
 							<h2 className="fitemTitle font-semibold text-2xl">Gender:</h2>
 							<div className="fitemSmall item1 mt-5 font-semibold text-1xl"> Select Gender</div>
 						</div> */}
-
+					
 						<div className="relative w-full mb-3">
-							<label className="block uppercase text-blueGray-600 text-xs font-bold mb-2" htmlFor="grid-password">
-								Size
+							<label className="block  text-white text-s font-bold mb-2" htmlFor="grid-password">
+								Size:
 							</label>
 							<input
 								type="text"
-								className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
+								className="font-bold border-2 border-white px-3 py-3 placeholder-gray-500 text-white bg-black rounded-lg text-sm shadow focus:outline-none focus:ring ease-linear transition-all duration-150"
 								placeholder="Enter Size"
 								value={size}
 								onChange={(e) => setSize(e.target.value)}
@@ -243,61 +260,66 @@ export default function Index() {
 						</div>
 
 						<div className="relative w-full mb-3">
-							<label className="block uppercase text-blueGray-600 text-xs font-bold mb-2" htmlFor="grid-password">
-								Color
+							<label className="block  text-white text-s font-bold mb-2" htmlFor="grid-password">
+								Color:
 							</label>
 							<input
 								type="text"
-								className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
+								className="font-bold border-2 border-white px-3 py-3 placeholder-gray-500 text-white bg-black rounded-lg text-sm shadow focus:outline-none focus:ring ease-linear transition-all duration-150"
 								placeholder="Enter Color"
 								value={color}
 								onChange={(e) => setColor(e.target.value)}
 							/>
 						</div>
 						<div className="relative w-full mb-3">
-							<label className="block uppercase text-blueGray-600 text-xs font-bold mb-2" htmlFor="grid-password">
-								Design Type
+							<label className="block  text-white text-s font-bold mb-2" htmlFor="grid-password">
+								Design Type:
 							</label>
 							<input
 								type="text"
-								className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
+								className="font-bold border-2 border-white px-3 py-3 placeholder-gray-500 text-white bg-black rounded-lg text-sm shadow focus:outline-none focus:ring ease-linear transition-all duration-150"
 								placeholder="Select Design"
 								value={design}
 								onChange={(e) => setDesign(e.target.value)}
 							/>
 						</div>
+					</div>
+					
+					<div className="fcontainerSecond text-left p-x-2">
 						<div className="relative w-full mb-3">
-							<label className="block uppercase text-blueGray-600 text-xs font-bold mb-2" htmlFor="grid-password">
-								Country of Manufacture
+							<label className="block  text-white text-s font-bold mb-2" htmlFor="grid-password">
+								Country of Manufacture:
 							</label>
 							<input
 								type="text"
-								className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
+								className="font-bold border-2 border-white px-3 py-3 placeholder-gray-500 text-white bg-black rounded-lg text-sm shadow focus:outline-none focus:ring ease-linear transition-all duration-150"
 								placeholder="Select Country"
 								value={country}
 								onChange={(e) => setCountry(e.target.value)}
 							/>
 						</div>
+						
 						<div className="relative w-full mb-3">
-							<label className="block uppercase text-blueGray-600 text-xs font-bold mb-2" htmlFor="grid-password">
-								Gender
+							<label className="block  text-white text-s font-bold mb-2" htmlFor="grid-password">
+								Gender:
 							</label>
 							<input
 								type="text"
-								className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
+								className="font-bold border-2 border-white px-3 py-3 placeholder-gray-500 text-white bg-black rounded-lg text-sm shadow focus:outline-none focus:ring ease-linear transition-all duration-150"
 								placeholder="Select Gender"
 								value={gender}
 								onChange={(e) => setGender(e.target.value)}
 							/>
 						</div>
+						
 					</div>
 
 					<div></div>
 				</div>
 			</section>
-			<div className="fconButton justify-center text-center">
-				<button onClick={handleMintNft} className="fitemMediumNext justify-center text-center font-semibold text-2xl mt-16">
-					MINT IT!!!
+			<div className="fconButton justify-center text-center bg-gray-900">
+				<button onClick={handleMintNft} className="fitemMediumNext justify-center text-center font-bold text-2xl mt-4">
+					MINT IT
 				</button>
 			</div>
 		</>
