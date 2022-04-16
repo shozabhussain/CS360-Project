cf2c524c9979a42dc23b8c56f8767abe72c8d83d
Suleman Mahmood <sulemanmahmood99@gmail.com>
2022-04-16 21:30:23 +0500

    fixed user menu button

--- layouts/Dashboard.js
+++ layouts/Dashboard.js
@@ -4,7 +4,6 @@ import UserMenuDropdown from "components/Dropdowns/usermenudropdown";
 
 // components
 
-import AdminNavbar from "components/Navbars/AdminNavbar.js";
 import Sidebar from "components/Sidebar/Sidebar.js";
 // import HeaderStats from "components/Headers/HeaderStats.js";
 // import FooterAdmin from "components/Footers/FooterAdmin.js";
@@ -13,12 +12,13 @@ export default function Admin({ children }) {
 	return (
 		<>
 			<Sidebar />
-			{/* <AdminNavbar /> */}
-			<div className="relative md:ml-64 ">
+			<UserMenuDropdown className=" z-50 top-0 right-0 fixed" />
+
+			<div className="relative md:ml-64 bg-blueGray-100">
 				{/* <AdminNavbar /> */}
 				{/* Header */}
 				{/* <HeaderStats /> */}
-				<div className="w-full px-4 mx-auto -m-24 md:px-10">
+				<div className="px-4 md:px-10 mx-auto w-full -m-24">
 					{children}
 					{/* <FooterAdmin /> */}
 				</div>
