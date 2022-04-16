502ea0d29da540a7d5d22cb585294418d4d084cf
Suleman Mahmood <sulemanmahmood99@gmail.com>
2022-04-17 00:02:22 +0500

    merged changes into main and completed pending verification proposal implementaion and fixed bugs

--- pages/dashboard/index.js
+++ pages/dashboard/index.js
@@ -35,14 +35,6 @@ export default function Index() {
 					</div>
 				</div>
 			</section>
-
-			<section className="pt-48 pb-40 text-white">
-				<div className="justify-center text-center flex flex-wrap mt-24">
-					<div className="w-full md:w-6/12 px-12 md:px-4">
-						<h2 className="font-semibold text-4xl">Edit Profile</h2>
-					</div>
-				</div>
-			</section>
 		</>
 	);
 }
--- pages/dashboard/view-nfts.js
+++ pages/dashboard/view-nfts.js
@@ -31,43 +31,6 @@ export default function Index() {
 	return (
 		<>
 			<section className="mt-48 pb-40">
-				<div className="justify-center text-center flex flex-wrap mt-24 pb-20">
-					<div className="w-full md:w-6/12 px-12 md:px-4">
-						<h2 className="font-semibold text-4xl">NFT COLLECTION</h2>
-					</div>
-				</div>
-
-				<div className="fcontainer">
-					<div className="fitem">
-						<div className="fitemSmall font-semibold text-1xl">
-							<h2 className="font-semibold text-1xl mt-6">NFT Image</h2>
-						</div>
-						<div className="font-semibold text-0xl">NFT Name</div>
-						<div className="font-semibold text-0xl">Other Metadata...</div>
-					</div>
-					<div className="fitem">
-						<div className="fitemSmall font-semibold text-1xl">
-							<h2 className="font-semibold text-1xl mt-6">NFT Image</h2>
-						</div>
-						<div className="font-semibold text-0xl">NFT Name</div>
-						<div className="font-semibold text-0xl">Other Metadata...</div>
-					</div>
-					<div className="fitem">
-						<div className="fitemSmall font-semibold text-1xl">
-							<h2 className="font-semibold text-1xl mt-6">NFT Image</h2>
-						</div>
-						<div className="font-semibold text-0xl">NFT Name</div>
-						<div className="font-semibold text-0xl">Other Metadata...</div>
-					</div>
-					<div className="fitem">
-						<div className="fitemSmall font-semibold text-1xl">
-							<h2 className="font-semibold text-1xl mt-6">NFT Image</h2>
-						</div>
-						<div className="font-semibold text-0xl">NFT Name</div>
-						<div className="font-semibold text-0xl">Other Metadata...</div>
-					</div>
-				</div>
-
 				<div>
 					<div className="flex flex-auto mt-48 custom-txt-mainheading justify-center">NFT COLLECTION</div>
 
